4edaf748fd6e05e5fa7761b39c9f37490bc2c8eb
Alexander-Poganatz <a_poganatz@outlook.com>
2018-05-02 13:06:45 -0400

    Incomplete implementation of ncursesw version, starting point

--- TerminalUnite/ConsoleAPIncurseswImplementation.cpp
+++ TerminalUnite/ConsoleAPIncurseswImplementation.cpp
@@ -1,4 +1,4 @@
-#include "../output/include/ConsoleAPI.hpp"
+#include <ConsoleAPI.hpp>
 
 #include <ncursesw/ncurses.h>
 #include <locale>
@@ -57,7 +57,6 @@ namespace apoganatz
 		private:
 
 		struct sigaction sa;
-
 		public:
 
 		NCursesWConsole()
@@ -70,7 +69,8 @@ namespace apoganatz
 			noecho();
 			cbreak();
 			keypad(stdscr, TRUE);
-			mousemask(ALL_MOUSE_EVENTS, NULL);			
+			mousemask(ALL_MOUSE_EVENTS, NULL);
+			timeout(10);			
 		}
 
 		~NCursesWConsole()
@@ -133,8 +133,130 @@ namespace apoganatz
 
 		size_t getInput(std::vector<InputEvent> & buffer)
 		{
+			int ch = getch();
+			if(ch == ERR)
+				return 0;
+			if(buffer.size() < 2)
+				buffer.resize(2);
+
+			// todo check for utf-8 input
+
+			int numToReturn = 1;
+			buffer[0].ch = ch;
+			buffer[0].eventCode = EventCode::ANY;	
 			
-			return 1;
+			switch(ch)
+			{
+				case 27: // Esc in both ASCII and Unicode
+					buffer[0].eventCode = EventCode::ESC; // I hope it is escape
+					break;
+				case KEY_UP:
+					buffer[0].eventCode = EventCode::UP;
+					break;
+				case KEY_DOWN:
+					buffer[0].eventCode = EventCode::DOWN;
+					break;
+				case KEY_LEFT:
+					buffer[0].eventCode = EventCode::LEFT;
+					break;
+				case KEY_RIGHT:
+					buffer[0].eventCode = EventCode::RIGHT;
+					break;
+				case 10: // Enter
+					buffer[0].eventCode = EventCode::ENTER;
+					break;
+				case KEY_BACKSPACE:
+					buffer[0].eventCode = EventCode::BACKSPACE;
+					break;
+				case KEY_DC:
+					buffer[0].eventCode = EventCode::DELETE;
+					break;
+				case KEY_IC:
+					buffer[0].eventCode = EventCode::INSERT;
+					break;
+				case KEY_NPAGE:
+					buffer[0].eventCode = EventCode::PAGE_DOWN;
+					break;
+				case KEY_PPAGE:
+					buffer[0].eventCode = EventCode::PAGE_UP;
+					break;
+				case KEY_HOME:
+					buffer[0].eventCode = EventCode::HOME;
+					break;
+				case KEY_END: // ???
+					buffer[0].eventCode = EventCode::END;
+					break;
+				case KEY_MOUSE:
+					MEVENT event;
+					if(getmouse(&event) == OK)
+					{
+						buffer[0].x = event.x;
+						buffer[0].y = event.y;
+
+						// bstate holds lots of information.
+						if(event.bstate&BUTTON1_PRESSED)
+							buffer[0].eventCode = EventCode::MOUSE_LEFT_CLICK;
+						if(event.bstate&BUTTON1_RELEASED)
+							buffer[0].eventCode = EventCode::MOUSE_LEFT_RELEASE;
+						if(event.bstate&BUTTON2_PRESSED)
+							buffer[0].eventCode = EventCode::MOUSE_SCROLLWHEEL_PRESS;
+						if(event.bstate&BUTTON2_RELEASED)
+							buffer[0].eventCode = EventCode::MOUSE_SCROLLWHEEL_RELEASE;
+						if(event.bstate&BUTTON3_PRESSED)
+							buffer[0].eventCode = EventCode::MOUSE_RIGHT_CLICK;
+						if(event.bstate&BUTTON3_RELEASED)
+							buffer[0].eventCode = EventCode::MOUSE_RIGHT_RELEASE;
+						if(event.bstate&BUTTON1_CLICKED)
+						{
+							buffer[0].eventCode = EventCode::MOUSE_LEFT_CLICK;
+							buffer[1].eventCode = EventCode::MOUSE_LEFT_RELEASE;
+							numToReturn = 2;
+						}
+						if(event.bstate&BUTTON2_CLICKED)
+						{
+							buffer[0].eventCode = EventCode::MOUSE_SCROLLWHEEL_PRESS;
+							buffer[1].eventCode = EventCode::MOUSE_SCROLLWHEEL_RELEASE;
+							numToReturn = 2;
+						}
+						if(event.bstate&BUTTON3_CLICKED)
+						{
+							buffer[0].eventCode = EventCode::MOUSE_RIGHT_CLICK;
+							buffer[1].eventCode = EventCode::MOUSE_RIGHT_RELEASE;
+							numToReturn = 2;
+						}
+					}
+					else
+					{
+						return 0;
+					}
+					break;
+			}
+			if(ch == KEY_F(1))
+				buffer[0].eventCode = EventCode::F1;
+			else if (ch == KEY_F(2))
+				buffer[0].eventCode = EventCode::F2;
+			else if(ch == KEY_F(3))
+				buffer[0].eventCode = EventCode::F3;
+			else if(ch == KEY_F(4))
+				buffer[0].eventCode = EventCode::F4; 	
+			else if(ch == KEY_F(5))      
+				buffer[0].eventCode = EventCode::F5;
+       			else if(ch == KEY_F(6))                       		
+       				buffer[0].eventCode = EventCode::F6;
+			else if(ch == KEY_F(7))      
+				buffer[0].eventCode = EventCode::F7;
+			else if(ch == KEY_F(8))      
+				buffer[0].eventCode = EventCode::F8;
+			else if(ch == KEY_F(9))      
+				buffer[0].eventCode = EventCode::F9;
+			else if(ch == KEY_F(10))      
+				buffer[0].eventCode = EventCode::F10;
+			else if(ch == KEY_F(11))      
+				buffer[0].eventCode = EventCode::F11;
+			else if(ch == KEY_F(12))      
+				buffer[0].eventCode = EventCode::F12;
+
+			return numToReturn;
 		}
 
 		void setCTRLCHandler(void(*fn)(void))
